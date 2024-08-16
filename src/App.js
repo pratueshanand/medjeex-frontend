575572307660bd5705b90d04a02cfc97daf17b45
NIVAS-kr <20bcs1180@cuchd.in>
2024-08-16 12:01:39 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div className="App">
-     Hii saar welcome to production level
+     <Register/>
     </div>
   );
 }
